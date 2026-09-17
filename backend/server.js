f8233c3514e8001597b3509ec1a4e6e9ef7d66ee
Sherwynm0702 <sherwynmp07@gmail.com>
2026-09-17 12:32:58 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ import cors from "cors";
 
 import pool from "./config/db.js";
 import authRoutes from "./routes/authroutes.js";
-console.log("DATABASE_URL at runtime:", process.env.DATABASE_URL);
 import applicationRoutes from "./routes/applicationRoutes.js";
 
 const PORT= process.env.PORT || 5000;
